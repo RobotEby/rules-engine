bb732e0d86fb866353474c05c714710d3bfca444
Kerlon Amaral <ubuntu.onion@hotmail.com>
2026-09-14 18:48:03 -0300

    feat: adds the index.ts file to export the main modules

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export * from "./types.js";
+export { RulesEngine } from "./engine.js";
+export { validateRuleSet } from "./validation.js";
+export { evaluateCondition, getByPath } from "./conditions.js";
